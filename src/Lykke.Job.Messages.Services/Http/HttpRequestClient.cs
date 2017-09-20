5745e865521fad5a6f3ca342c3376dd510b8757e
Sergey Nesterov <sergey.nesterov@lykke.com>
2017-09-20 18:10:10 +0300

    Nuget updates to fix RazorLight issue

--- src/Lykke.Job.Messages.Services/Http/HttpRequestClient.cs
+++ src/Lykke.Job.Messages.Services/Http/HttpRequestClient.cs
@@ -5,22 +5,6 @@ namespace Lykke.Job.Messages.Services.Http
 {
     public class HttpRequestClient
     {
-        public async Task<string> GetRequest(string url)
-        {
-            var restClient = new RestClient(url);
-
-            var request = new RestRequest("", Method.GET);
-            request.AddHeader("Accept", "application/json");
-
-            var taskCompletion = new TaskCompletionSource<IRestResponse>();
-
-            restClient.ExecuteAsync(request, r => taskCompletion.SetResult(r));
-
-            var response = (RestResponse)(await taskCompletion.Task);
-
-            return response.Content;
-        }
-
         public async Task<string> PostRequest(string data, string url)
         {
             var restClient = new RestClient(url);
