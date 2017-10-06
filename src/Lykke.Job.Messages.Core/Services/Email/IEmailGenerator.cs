d1f9a6f0f693fa5f4c7e87969cb391e70c4a4834
Dmitry Samodov <samodov.dmitry@gmail.com>
2017-10-06 19:22:10 +0300

    added method GenerateRegistrationVerifyEmailMsg

--- src/Lykke.Job.Messages.Core/Services/Email/IEmailGenerator.cs
+++ src/Lykke.Job.Messages.Core/Services/Email/IEmailGenerator.cs
@@ -34,5 +34,6 @@ namespace Lykke.Job.Messages.Core.Services.Email
         Task<EmailMessage> GenerateSwiftCashOutRequestMsg(string partnerId, SwiftCashOutRequestData messageData);
         Task<EmailMessage> GenerateRequestForDocumentMsg(string partnerId, RequestForDocumentData messageData);
         Task<EmailMessage> GenerateSwiftCashoutProcessedMsg(string partnerId, SwiftCashoutProcessedData messageData);
+        Task<EmailMessage> GenerateRegistrationVerifyEmailMsg(string partnerId, RegistrationEmailVerifyData messageData);
     }
 }
