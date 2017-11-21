167cd83f3ee6b4078522d452f1efffaace7e52be
Yury Batsyuro <yury.batsyuro@lykke.com>
2017-11-21 21:25:23 +0300

    Replaced EmailFormatter with TemplateFormatter and added PartnerId to SMS

--- src/Lykke.Job.Messages.Core/Services/Sms/ISmsTextGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Lykke.Job.Messages.Core.Services.Sms
-{
-    public interface ISmsTextGenerator
-    {
-        Task<string> GenerateConfirmSmsText(string confirmationCode);
-    }
-}
