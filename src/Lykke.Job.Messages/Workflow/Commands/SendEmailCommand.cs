7238986f4dce215fe607f49c1ce29375b8718cb7
AleksandrBelousov <tortyt1@gmail.com>
2018-03-13 15:19:23 +0400

    Added Console for testing

--- src/Lykke.Job.Messages/Workflow/Commands/SendEmailCommand.cs
+++ src/Lykke.Job.Messages/Workflow/Commands/SendEmailCommand.cs
@@ -6,16 +6,13 @@ namespace Lykke.Job.Messages.Workflow.Commands
     /// <summary>
     /// Command to send email message
     /// </summary>
-    [MessagePackObject]
+    [MessagePackObject(keyAsPropertyName:true)]
     public class SendEmailCommand<T> where T : IEmailMessageData
     {
-        [Key(0)]
         public string PartnerId { get; set; }
 
-        [Key(1)]
         public string EmailAddress { get; set; }
 
-        [Key(2)]
         public T MessageData { get; set; }
     }
 }
---
+++ tests/Lykke.Job.Messages.Tests.Console/Lykke.Job.Messages.Tests.Console/Program.cs
@@ -0,0 +1,41 @@
+﻿using Lykke.Cqrs;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Lykke.Job.Messages.Workflow.Commands;
+using Lykke.Job.Messages.Contract.Emails.MessageData;
+using Lykke.Job.Messages.Contract;
+
+namespace Lykke.Job.Messages.Tests.Console
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var webHostBuilder = CreateWebHost(args);
+            var webHost = webHostBuilder.Build();
+            var cqrsEngine = webHost.Services.GetService<ICqrsEngine>();
+
+            cqrsEngine.SendCommand(new SendEmailCommand<NoRefundOCashOutData>()
+            {
+                EmailAddress = "tortyt1@gmail.com",
+                MessageData = new NoRefundOCashOutData()
+                {
+                    Amount = 10,
+                    AssetId = "ETH",
+                    SrcBlockchainHash = "0x00000000000000000000000000..."
+                },
+                PartnerId = null
+            },
+            EmailMessagesBoundedContext.Name,
+            EmailMessagesBoundedContext.Name);
+            webHost.Run();
+            
+        }
+
+        public static IWebHostBuilder CreateWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
