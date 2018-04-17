af3af9ff938d7ec51077b73ae8b82480364c3d01
Mikhail Musin <mikhail.musin@gmail.com>
2018-04-18 00:55:42 +0300

    LWDEV-6945 terminal session events

---
+++ src/Lykke.Job.Messages/Sagas/TerminalSessionsSaga.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Lykke.Cqrs;
+using Lykke.Service.ClientAccount.Client;
+using Lykke.Service.PushNotifications.Contract.Commands;
+using Lykke.Service.Session.Contracts;
+
+namespace Lykke.Job.Messages.Sagas
+{
+    public class TerminalSessionsSaga
+    {
+        private readonly IClientAccountClient _clientAccountClient;
+
+        public TerminalSessionsSaga(IClientAccountClient clientAccountClient)
+        {
+            _clientAccountClient = clientAccountClient;
+        }
+
+        [UsedImplicitly]
+        public async Task Handle(TradingSessionCreatedEvent evt, ICommandSender sender)
+        {
+            var command = new DataNotificationCommand
+            {
+                NotificationIds = new [] { (await _clientAccountClient.GetByIdAsync(evt.ClientId)).NotificationsId },
+                Type = "TradingSessionCreated"
+            };
+
+            sender.SendCommand(command, "push-notifications");
+        }
+    }
+}
