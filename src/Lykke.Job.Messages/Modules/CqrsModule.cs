650fe3e5d5365766e11937fad9f96e7bf7f9c046
Mikhail Musin <mikhail.musin@gmail.com>
2018-04-18 01:45:37 +0300

    LWDEV-6242

--- src/Lykke.Job.Messages/Modules/CqrsModule.cs
+++ src/Lykke.Job.Messages/Modules/CqrsModule.cs
@@ -41,8 +41,9 @@ namespace Lykke.Job.Messages.Modules
             builder.Register(context => new AutofacDependencyResolver(context)).As<IDependencyResolver>();
 
             builder.RegisterType<BlockchainOperationsSaga>().SingleInstance();
-            builder.RegisterType<LoginNotificationsSaga>().SingleInstance();
-            
+            builder.RegisterType<LoginEmailNotificationsSaga>().SingleInstance();
+            builder.RegisterType<LoginPushNotificationsSaga>().SingleInstance();
+
             var messagingEngine = new MessagingEngine(_log,
                 new TransportResolver(new Dictionary<string, TransportInfo>
                 {
@@ -77,11 +78,17 @@ namespace Lykke.Job.Messages.Modules
                     true,
                     Register.DefaultEndpointResolver(clientEndpointResolver),
 
-                    Register.Saga<LoginNotificationsSaga>("login-notifications-saga")
+                    Register.Saga<LoginEmailNotificationsSaga>("login-email-notifications-saga")
                         .ListeningEvents(typeof(ClientLoggedEvent))
                             .From("registration").On(eventsRoute)
                         .PublishingCommands(typeof(SendEmailCommand)).To("email").With(commandsRoute)
                             .ProcessingOptions(commandsRoute).MultiThreaded(2).QueueCapacity(256),
+
+                    Register.Saga<LoginPushNotificationsSaga>("login-push-notifications-saga")
+                        .ListeningEvents(typeof(ClientLoggedEvent))
+                        .From("registration").On(eventsRoute)
+                        .PublishingCommands(typeof(TextNotificationCommand)).To("push-notifications").With(commandsRoute)
+                        .ProcessingOptions(commandsRoute).MultiThreaded(2).QueueCapacity(256),
 
                     Register.Saga<BlockchainOperationsSaga>("blockchain-notification-saga")
                         .ListeningEvents(typeof(CashinCompletedEvent), typeof(CashoutCompletedEvent))
