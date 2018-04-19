237977873b0de5b560f80505d4b1b9eceb98ff46
igorag <igorag@mmtr.ru>
2018-04-19 07:07:09 +0300

    LWDEV-6958-withdrawal-requested

---
+++ src/Lykke.Job.Messages.Contract/Lykke.Job.Messages.Contract/Emails/MessageData/SwiftCashoutRequestedData.cs
@@ -0,0 +1,10 @@
+﻿namespace Lykke.Job.Messages.Contract.Emails.MessageData
+{
+    public class SwiftCashoutRequestedData : IEmailMessageData
+    {
+        public const string EmailTemplateId = "SwiftCashoutRequested";
+
+        public string FullName { get; set; }
+        public string Year { get; set; }
+    }
+}
