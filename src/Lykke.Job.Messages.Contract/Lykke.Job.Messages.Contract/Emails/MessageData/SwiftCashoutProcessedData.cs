a42d7174a760d5ee6f87919563c978e7126ff05b
igorag <igorag@mmtr.ru>
2018-04-26 15:14:25 +0300

    LWDEV-6923-Withdrawal-Email

--- src/Lykke.Job.Messages.Contract/Lykke.Job.Messages.Contract/Emails/MessageData/SwiftCashoutProcessedData.cs
+++ src/Lykke.Job.Messages.Contract/Lykke.Job.Messages.Contract/Emails/MessageData/SwiftCashoutProcessedData.cs
@@ -5,6 +5,8 @@
         public const string EmailTemplateId = "SwiftCashoutProcessed";
 
         public string FullName { get; set; }
+        public string DateOfWithdrawal { get; set; }
+        
         public string Year { get; set; }
     }
 }
